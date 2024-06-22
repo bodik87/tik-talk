3c2199ddd467c59d5b94936d0a53b53a46dc3ea9
Bogdan <bsshul@gmail.com>
2024-06-22 16:14:33 +0200

    rgds

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ProfileCardComponent } from './common-ui/profile-card/profile-card.component';
+import { ProfileService } from './data/services/profile.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  imports: [RouterOutlet, ProfileCardComponent],
 })
 export class AppComponent {
   title = 'tik-talk';
+  profileService = inject(ProfileService);
+  profiles: any = [];
+
+  constructor() {
+    this.profileService
+      .getTestAccounts()
+      .subscribe((val) => (this.profiles = val));
+  }
 }
